e040c02add04df733f7fac0b1d9e66f58aca6279
monarchvakani26 <moanrchvakani05@gmail.com>
2025-07-16 01:33:21 +0530

    Initial clean push with frontend and backend

--- skill-swap-backend/index.js
+++ skill-swap-backend/index.js
@@ -67,7 +67,7 @@ app.use(passport.session());
 app.use("/uploads", express.static("uploads"));
 app.use("/api/auth", authRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
