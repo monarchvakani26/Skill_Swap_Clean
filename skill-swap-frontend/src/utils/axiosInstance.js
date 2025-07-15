41de953b8017273adbac7caf2e617d1cbe6b674e
monarchvakani26 <moanrchvakani05@gmail.com>
2025-07-16 02:42:10 +0530

    redirecion urls

--- skill-swap-frontend/src/utils/axiosInstance.js
+++ skill-swap-frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL + "/api",
+ BASE_URL : import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
 });
 
