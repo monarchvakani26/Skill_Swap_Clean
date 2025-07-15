50594ff944eb23697cc3524dfa3f76b33bbfc32f
monarchvakani26 <moanrchvakani05@gmail.com>
2025-07-16 02:36:16 +0530

    redirecion urls

--- skill-swap-frontend/src/utils/axiosInstance.js
+++ skill-swap-frontend/src/utils/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const instance = axios.create({
-  baseURL: "http://localhost:5000/api", // your backend URL
-  withCredentials: true, // IMPORTANT: allows cookies to be sent
+const API = axios.create({
+  baseURL: import.meta.env.VITE_BACKEND_URL + "/api",
+  withCredentials: true,
 });
 
 export default instance;
