e4a3ea1f400d03d2d76372308cd2f0ebfc14dfc0
monarchvakani26 <moanrchvakani05@gmail.com>
2025-07-16 02:55:26 +0530

    redirecion urls 3

--- skill-swap-frontend/src/utils/axiosInstance.js
+++ skill-swap-frontend/src/utils/axiosInstance.js
@@ -3,8 +3,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
+  baseURL: import.meta.env.VITE_BACKEND_URL, // ✅ Use baseURL, not BASE_URL
   withCredentials: true,
 });
+
+
 
 export default instance;
